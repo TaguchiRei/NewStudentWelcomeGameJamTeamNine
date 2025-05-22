b00503e577f8a3612a816b77e044595dba8d6726
TaguchiRei <ren.len2426@gmail.com>
2025-05-22 12:38:19 +0900

    [Add]残り時間を表示するスクリプト追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -25,6 +25,7 @@ public class GameManager : MonoBehaviour
         if(!_timerStarted) return;
         
         _timer -= Time.deltaTime;
+        _timeText.text = _timer.ToString();
         if (_timer <= 0)
         {
             
