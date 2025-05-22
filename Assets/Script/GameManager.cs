01b23b372eb06105bfb059640de4517dd1aabda7
TaguchiRei <ren.len2426@gmail.com>
2025-05-22 12:29:27 +0900

    [Add]タイマー作成

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -20,6 +20,17 @@ public class GameManager : MonoBehaviour
         StartCoroutine(CountDown());
     }
 
+    private void Update()
+    {
+        if(!_timerStarted) return;
+        
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            
+        }
+    }
+
     public int Score
     {
         get;
@@ -41,5 +52,6 @@ public class GameManager : MonoBehaviour
         _countDownText.text = "Start!";
         yield return new WaitForSeconds(1f);
         _countDownText.enabled = false;
+        _timerStarted = true;
     }
 }
