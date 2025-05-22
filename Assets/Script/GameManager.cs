7244081cdd127d08a175131b069bbcdf70f46f5c
TaguchiRei <ren.len2426@gmail.com>
2025-05-22 12:25:37 +0900

    [Add]ゲーム開始時のカウントダウンを作成

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]private float _timeLimit;
+    [SerializeField] private Text _timeText;
+    [SerializeField] private Text _countDownText;
+    private float _timer;
+
+    private bool _timerStarted = false;
+
+    private void Start()
+    {
+        _timer = _timeLimit;
+        StartCoroutine(CountDown());
+    }
+
     public int Score
     {
         get;
@@ -13,5 +29,17 @@ public class GameManager : MonoBehaviour
     private void AddScore(int score, int magnification)
     {
         Score += score * magnification;
+    }
+
+    IEnumerator CountDown()
+    {
+        for (int i = 3; i > 0 ; i--)
+        {
+            _countDownText.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+        _countDownText.text = "Start!";
+        yield return new WaitForSeconds(1f);
+        _countDownText.enabled = false;
     }
 }
