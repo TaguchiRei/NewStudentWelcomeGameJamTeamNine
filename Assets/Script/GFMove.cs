d5661dcbb9204c8ccbd76059f2f2e4134abc6317
kosei291780 <kosei291780@gmail.com>
2025-05-23 13:31:01 +0900

    no message

--- Assets/Script/GFMove.cs
+++ Assets/Script/GFMove.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+>>>>>>> Stashed changes
 
 public class GFMove : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] Vector3 _targetPos;
+<<<<<<< Updated upstream
+=======
+    [SerializeField] Talk[] _talkDate;
+
+
+    private GameObject _targetObject;
+>>>>>>> Stashed changes
     private Rigidbody2D rb;
 
     bool _mouseClick ;
@@ -31,11 +43,39 @@ public class GFMove : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Love"))
         {
+<<<<<<< Updated upstream
         }
         if (collision.gameObject.CompareTag("Keep"))
         {
         }
         Destroy(gameObject);//このゲームオブジェクトを消滅させる
+=======
+            OnGoal?.Invoke(Money, 1);
+            var index = Random.Range(0, _talkDate[0].talkData.Count);
+
+            _textMesh.text = _talkDate[0].talkData[index].Talk;
+        }
+        if (collision.gameObject.CompareTag("Keep"))
+        {
+            OnGoal?.Invoke(Money, 0.5f);
+
+            var index = Random.Range(0, _talkDate[1].talkData.Count);
+
+            _textMesh.text = _talkDate[1].talkData[index].Talk;
+        }
+        if (collision.gameObject.CompareTag("break"))
+        {
+            OnGoal?.Invoke(Money, 0.5f);
+            var index = Random.Range(0, _talkDate[2].talkData.Count);
+
+            _textMesh.text = _talkDate[2].talkData[index].Talk;
+        }
+        _isGoaled = true;
+        rb.velocity = Vector2.zero;
+        _textBox.SetActive(true);
+        Invoke(nameof(DestroyObject),1f);
+        
+>>>>>>> Stashed changes
     }
     void OnMouseDrag()
     {
